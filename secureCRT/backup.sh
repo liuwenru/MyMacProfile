7f3580ba5955d2364e3dd198ffae241ee3a3b0c1
ijarvis <ijarvis@sina.com>
2017-12-17 14:18:16 +0800

    init

---
+++ secureCRT/backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+cp -rf /Users/ijarvis/Library/Application\ Support/VanDyke/SecureCRT/Config/ButtonBarV4.ini  ./
+cp -rf /Users/ijarvis/Library/Application\ Support/VanDyke/SecureCRT/Config/Color\ Schemes.ini  ./
+cp -rf /Users/ijarvis/Library/Application\ Support/VanDyke/SecureCRT/Config/Global.ini  ./
+cp -rf /Users/ijarvis/Library/Application\ Support/VanDyke/SecureCRT/Config/SSH2.ini  ./
+
+
+
+
+
