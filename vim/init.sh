24930b378d9b9238b77daea276a99e9ad2ebbc16
ijarvis <ijarvis@sina.com>
2018-03-17 23:59:28 +0800

    add vim colors

---
+++ vim/init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: init.sh
+# 
+#         USAGE: ./init.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2018/03/17 23:56
+#      REVISION:  ---
+#===============================================================================
+
+
+
+ln -s ${pwd}/vim/colors  ~/.vim/colors 
+
+
