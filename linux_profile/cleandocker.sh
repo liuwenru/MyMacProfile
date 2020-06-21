423204cf4711c53c2c43cc1a3c4bb17fe4b78937
ijarvis <ijarvis@sina.com>
2020-06-21 23:14:22 +0800

    fix bug

--- linux_profile/cleandocker.sh
+++ linux_profile/cleandocker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker rm `docker ps -a | grep Exited | awk '{print $1}'`
+docker rmi -f  `docker images | grep '<none>' | awk '{print $3}'`
--- linux_profile/cleannoneimages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#docker images|grep none|awk '{print $3}'|xargs docker rmi
-
-docker rmi -f  `docker images | grep '<none>' | awk '{print $3}'`
