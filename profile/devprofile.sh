a61778cbc393b5bbb83038fbb17de0aace70befe
liuwenru <ijarvis@sina.com>
2021-03-14 13:04:38 +0800

    fix bug

--- profile/devprofile.sh
+++ profile/devprofile.sh
@@ -23,7 +23,19 @@ case ${envname} in
         ;;
     Darwin)
         echo -n " welcome Mac dev............."
-
+        export PATH=$HOME/bin:/usr/local/bin:$HOME/.local/bin:$PATH
+        export MANPATH="/usr/local/man:/Users/liuwenru/tools/mactools/dash_docs/linux_manpages:$MANPATH"
+        export GO111MODULE=on
+        export GOPATH=/Users/liuwenru/workspace/goworkspace
+        export PATH=$GOPATH/bin:$PATH
+        export ANT_HOME="/Users/liuwenru/tools/mactools/apache-ant-1.9.15"
+        export MVN_HOME="/Users/liuwenru/tools/mactools/apache-maven-3.6.3"
+        export GRADLE_HOME="/Users/liuwenru/tools/apps/gradle-6.8.2"
+        export PATH="$MVN_HOME/bin:$ANT_HOME/bin:$GRADLE_HOME/bin:$PATH"
+        export CFLAGS="-g -O0"
+        export CXXFLAGS="-g -O0"
+        export TERM=xterm-256color
+        export PATH=$PATH:$HOME/bin:/usr/local/vim/bin
         ;;
     *)
         ;;
